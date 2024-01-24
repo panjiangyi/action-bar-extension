abf7f5a71b58520788b94b83676aece518761c63
panjiangyi <78237724@qq.com>
2024-01-24 17:55:27 +0800

    ci: eslint

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,22 +1,25 @@
-console.log('background is running')
+console.log('background is running');
 
 const getCurrentTab = async () => {
-  const queryOptions = { active: true, currentWindow: true }
-  const [tab] = await chrome.tabs.query(queryOptions)
-  return tab
-}
+	const queryOptions = { active: true, currentWindow: true };
+	const [tab] = await chrome.tabs.query(queryOptions);
+	return tab;
+};
 
 chrome.runtime.onMessage.addListener((request) => {
-  if (request.type === 'COUNT') {
-    console.log('background has received a message from popup, and count is ', request?.count)
-  }
-})
+	if (request.type === 'COUNT') {
+		console.log(
+			'background has received a message from popup, and count is ',
+			request?.count,
+		);
+	}
+});
 // Listen for the open omni shortcut
 chrome.commands.onCommand.addListener(async (command) => {
-  if (command === 'open-arc') {
-    const tab = await getCurrentTab()
-    const tabId = tab?.id
-    if (tabId == null) return
-    chrome.tabs.sendMessage(tabId, { request: 'open-arc' })
-  }
-})
+	if (command === 'open-arc') {
+		const tab = await getCurrentTab();
+		const tabId = tab?.id;
+		if (tabId == null) return;
+		chrome.tabs.sendMessage(tabId, { request: 'open-arc' });
+	}
+});
