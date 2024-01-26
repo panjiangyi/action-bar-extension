1462cd5474064dbd44a691a6c92abadbe93f638b
panjiangyi <78237724@qq.com>
2024-01-26 10:53:24 +0800

    ci: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
-import { defineConfig } from 'vite'
-import { crx } from '@crxjs/vite-plugin'
-import react from '@vitejs/plugin-react-swc'
+import { crx } from '@crxjs/vite-plugin';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
-import manifest from './src/manifest'
+import manifest from './src/manifest';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    build: {
-      emptyOutDir: true,
-      outDir: 'build',
-      sourcemap: 'inline',
-      rollupOptions: {
-        output: {
-          chunkFileNames: 'assets/chunk-[hash].js',
-        },
-      },
-    },
+export default defineConfig({
+	build: {
+		emptyOutDir: true,
+		outDir: 'build',
+		sourcemap: 'inline',
+		rollupOptions: {
+			output: {
+				chunkFileNames: 'assets/chunk-[hash].js',
+			},
+		},
+	},
 
-    plugins: [crx({ manifest }), react({ plugins: [['@swc-jotai/react-refresh', {}]] })],
-  }
-})
+	plugins: [
+		crx({ manifest }),
+		react({ plugins: [['@swc-jotai/react-refresh', {}]] }),
+	],
+});
